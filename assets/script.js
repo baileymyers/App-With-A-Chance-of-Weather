21425babc0b7b32e742d697718ee30cfbb5161f2
Bailey <baileymyers96@gmail.com>
2020-10-10 09:51:29 -0400

    nothing is working!

--- assets/script.js
+++ assets/script.js
@@ -16,21 +16,21 @@ $(".search-btn").on("click", function() {
     let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Tacoma&appid=d3578c0285de5e61bde169ab3e1ff031";
 
     $.ajax({ url: apiUrl, method: "GET" }).then(function (response) {
-            const cityEl = $(".city");
-            const windEl = $(".wind");
-            const humidityEl = $(".humidity");
-            const tempEl = $(".temp");
+            // const cityEl = $(".city");
+            // const windEl = $(".wind");
+            // const humidityEl = $(".humidity");
+            // const tempEl = $(".temp");
 
             console.log(response);
 
-            cityEl.text(response.name);
-            windEl.text(response.wind.speed + "m/s");
-            humidityEl.text(response.main.humidity + "%");
-            tempEl.html(convertKtoF(parseFloat(response.main.temp)) + "&deg;F");
+            // cityEl.text(response.name);
+            // windEl.text(response.wind.speed + "m/s");
+            // humidityEl.text(response.main.humidity + "%");
+            // tempEl.html(convertKtoF(parseFloat(response.main.temp)) + "&deg;F");
         });
     
-    function convertKtoF(tempInKelvin) {
-        // (360K − 273.15) × 9/5 + 32 = 188.33°F
-        return ((tempInKelvin - 273.15) * 9) / 5 + 32;
-        };
+    // function convertKtoF(tempInKelvin) {
+    //     // (360K − 273.15) × 9/5 + 32 = 188.33°F
+    //     return ((tempInKelvin - 273.15) * 9) / 5 + 32;
+    //     };
 })
