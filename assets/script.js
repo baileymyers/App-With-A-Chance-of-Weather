fcdcbfa8ccfaa625a27f042c70ba6ce7f2db6eef
Bailey <baileymyers96@gmail.com>
2020-10-12 20:28:46 -0700

    updated styling; made changes to html to adjust to new script that adds elements to search history

--- assets/script.js
+++ assets/script.js
@@ -31,10 +31,50 @@ $("#searchBtn").on("click", function() {
             windEl.text(response.wind.speed + "m/s");
             humidityEl.text(response.main.humidity + "%");
             tempEl.html(convertKtoF(parseFloat(response.main.temp)) + "&deg;F");
+
+            function convertKtoF(tempInKelvin) {
+                // (360K − 273.15) × 9/5 + 32 = 188.33°F
+                return ((tempInKelvin - 273.15) * 9) / 5 + 32;
+                };
+
+            let historyLocation = response.name;
+            console.log(historyLocation);
+            let history = $(".history-list").append("<button>");
+            let historyBtn = history.text(historyLocation);
+            $(historyBtn).addClass("list-group-item list-group-item-action border border-secondary rounded");
+            $(historyBtn).attr("type", "button");
+            $(historyBtn).val(historyLocation);
+
+            let latitude = response.coord.lat;
+            let longitude = response.coord.lon;
+
+            console.log(latitude);
+            console.log(longitude);
+
+            // http://api.openweathermap.org/data/2.5/uvi?lat={lat}&lon={lon}&appid={API key}
+
+            let uvApiUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
+
+            $.ajax({ url: uvApiUrl, method: "GET" }).then(function (response) {
+                console.log(response);
+                let uviEl = $(".uv-index");
+                uvIndex = uviEl.text(response.value);
+            });
         });
-    
-    function convertKtoF(tempInKelvin) {
-        // (360K − 273.15) × 9/5 + 32 = 188.33°F
-        return ((tempInKelvin - 273.15) * 9) / 5 + 32;
-        };
+
+        let uviElVal = $(".uv-index").val();
+        if (uviElVal >= 0 && uviElVal <= 2) {
+            document.getElementById("uv-index").style.backgroundColor = "green";
+            document.getElementById("uv-index").style.color = "white";
+            document.getElementById("uv-index").style.padding = "3px 5px";
+            $("#uv-index").addClass("rounded-lg");
+        } else if (uviElVal >= 3 && uviElVal <= 5) {
+            document.getElementById("uv-index").style.backgroundColor = "yellow";
+        } else if (uviElVal >= 6 && uviElVal <= 7) {
+            document.getElementById("uv-index").style.backgroundColor = "orange";
+        } else if (uviElVal >= 8 && uviElVal <= 10) {
+            document.getElementById("uv-index").style.backgroundColor = "red";
+        } else if (uviElVal >10) {
+            document.getElementById("uv-index").style.backgroundColor = "purple";
+        }
 });
