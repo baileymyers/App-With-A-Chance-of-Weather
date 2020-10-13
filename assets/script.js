8b4e9c2cd056387894dc9d41899a3dcc3d493247
Bailey <baileymyers96@gmail.com>
2020-10-12 18:28:16 -0700

    adding some updates

--- assets/script.js
+++ assets/script.js
@@ -5,32 +5,36 @@ When the screen is refreshed,
 then the search history is still shown
 */
 
-$(".search-btn").on("click", function() {
-    // let location = $(".search-input").val();
-    // let apiKey = "387132af451fb4380aeaf30be323dfab";
-    // let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + apiKey;
+$("#searchBtn").on("click", function() {
+    let location = $(".search-input").val();
+    let apiKey = "387132af451fb4380aeaf30be323dfab";
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&forecast?q=" + location + "&appid=" + apiKey;
 
-    // console.log(location);
-    // console.log(apiUrl);
-
-    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Tacoma&appid=d3578c0285de5e61bde169ab3e1ff031";
+    console.log(location);
+    console.log(apiUrl);
 
     $.ajax({ url: apiUrl, method: "GET" }).then(function (response) {
-            // const cityEl = $(".city");
-            // const windEl = $(".wind");
-            // const humidityEl = $(".humidity");
-            // const tempEl = $(".temp");
+            const cityEl = $(".city");
+            const windEl = $(".wind");
+            const humidityEl = $(".humidity");
+            const tempEl = $(".temp");
 
             console.log(response);
 
-            // cityEl.text(response.name);
-            // windEl.text(response.wind.speed + "m/s");
-            // humidityEl.text(response.main.humidity + "%");
-            // tempEl.html(convertKtoF(parseFloat(response.main.temp)) + "&deg;F");
+            let city = cityEl.text(response.name);
+            let date = moment().format(' (MMMM Do YYYY)');
+            let cityDate = city.append(date);
+            let icon = response.weather[0].icon;
+            let iconUrl= "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            $('#wicon').attr('src', iconUrl);
+            $('#wicon').attr('alt', "weather icon");
+            windEl.text(response.wind.speed + "m/s");
+            humidityEl.text(response.main.humidity + "%");
+            tempEl.html(convertKtoF(parseFloat(response.main.temp)) + "&deg;F");
         });
     
-    // function convertKtoF(tempInKelvin) {
-    //     // (360K − 273.15) × 9/5 + 32 = 188.33°F
-    //     return ((tempInKelvin - 273.15) * 9) / 5 + 32;
-    //     };
-})
+    function convertKtoF(tempInKelvin) {
+        // (360K − 273.15) × 9/5 + 32 = 188.33°F
+        return ((tempInKelvin - 273.15) * 9) / 5 + 32;
+        };
+});
